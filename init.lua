f456fafee5fad752cdc1d209a41670995ffb3817
rakshith <62304358+rakshith-git@users.noreply.github.com>
2024-03-12 20:23:30 +0530

    added minimap

--- init.lua
+++ init.lua
@@ -6,8 +6,8 @@ if vim.g.neovide then
   local alpha = function()
     return string.format("%x", math.floor((255 * vim.g.transparency) or 0.8))
   end
-  vim.g.neovide_transparency = 0.99
-  vim.g.transparency = 0.8
+  vim.g.neovide_transparency = 0.985
+  vim.g.transparency = 1.0
   vim.g.neovide_background_color = "#0f1117" .. alpha()
   vim.g.neovide_window_blurred = true
   vim.g.neovide_cursor_animate_in_insert_mode = true
@@ -18,11 +18,3 @@ if vim.g.neovide then
   vim.g.neovide_floating_blur_amount_x = 0.0
   vim.g.neovide_floating_blur_amount_y = 0.0
 end
-require("luasnip").setup({
-  snippets = {
-    all = {
-      -- Add the path to your snippets directory here
-      "~/.config/nvim/snippets/javascript.snippets",
-    },
-  },
-})
