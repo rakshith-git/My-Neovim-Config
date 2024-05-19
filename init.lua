104a3de071c39072b4c97e7fbfdc1196d3b76b87
rakshith <62304358+rakshith-git@users.noreply.github.com>
2024-05-20 01:10:35 +0530

    new changes

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ if vim.g.neovide then
   local alpha = function()
     return string.format("%x", math.floor((255 * vim.g.transparency) or 0.8))
   end
-  vim.g.neovide_transparency = 0.985
+  vim.g.neovide_transparency = 0.9
   vim.g.transparency = 1.0
   vim.g.neovide_background_color = "#0f1117" .. alpha()
   vim.g.neovide_window_blurred = true
@@ -17,4 +17,31 @@ if vim.g.neovide then
   vim.g.neovide_floating_shadow = false
   vim.g.neovide_floating_blur_amount_x = 0.0
   vim.g.neovide_floating_blur_amount_y = 0.0
+
+  vim.g.neovide_scale_factor = 1.0
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.1)
+  end)
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.1)
+  end)
+end
+if vim.g.nvui then
+  -- Configure through vim commands
+  vim.cmd([[NvuiCmdFontFamily FiraCode Nerd Font]])
+  vim.opt.guifont = "Hack Nerd Font Mono:h11"
+end
+
+function _G.compile_and_run_cpp()
+  -- Get the current file name
+  local file_name = vim.fn.expand("%:p:r")
+  -- Compile the C++ file
+  local compile_cmd = "g++ " .. file_name .. ".cpp -o " .. file_name
+  -- Run the compiled program
+  local run_cmd = "" .. file_name
+  -- Open a new toggleterm terminal and execute the commands
+  require("toggleterm").exec(compile_cmd .. " && " .. run_cmd)
 end
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/mahn/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>m", "<cmd>MinimapToggle<cr>", { desc = "Toggle Minimap" })
+vim.keymap.set("n", "<leader>m", "<cmd>UndotreeToggle<cr><cmd>UndotreeFocus<cr>", { desc = "Toggle undotree" })
+vim.keymap.set("i", "jj", "<Esc>", { desc = "leave insert mode" })
+vim.keymap.set("v", "<leader>cs", "<cmd>CodeSnap<cr>", { desc = "Take a Snap" })
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>cp",
+  ":lua compile_and_run_cpp()<CR>",
+  { desc = "run C++", noremap = true, silent = true }
+)
